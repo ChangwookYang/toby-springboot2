6350f6f064867e6a9c15fa53d7f1abad5faeaeb7
changwook yang <ycw92325@gmail.com>
2023-03-07 22:27:58 +0900

    Containerless 구현 - 서블릿 등록 및 매핑
    [Step 2 서블릿 등록 및 매핑]
    
    서블릿을 서블릿 컨테이너에 등록해주어야 한다.
    getWebServer로 webServer를 만들때에 ServletContextInitializer 에서 서블릿을 등록해준다. 
    즉 웹서버 구동 시에 어떤 서블릿이 서블릿 컨테이너에 등록될지 알아야한다.
    
    "hello"라는 서블릿을 등록한다. 
    첫번째 파라미터는 서블릿명이고 
    두번째는 HttpServlet을 상속하는 익명 클래스 오브젝트를 만들어서 service 메소드를 구현한다.
    
    servletContext.addServlet("hello", new HttpServlet() {
        @Override
        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                // service에서는 request, response가 있고 언제나 요청이 있어야 응답이 있다.
       }
    }.addMapping("/hello"); 
    
    그리고 urlPattern을 addMapping의 파라미터에 추가하여 
    서블릿 컨테이너가 어떤 요청에서 어느 서블릿에 매핑해야할지 결정할 수 있도록 매핑해준다.
    
    service 메소드에서 hello 서블릿에 대한 응답값도 정의할 수 있다. 
    웹 응답의 3대 요소 리턴 (상태코드 / 헤더 / 바디)
    resp.setStatus(200);
    resp.setHeader("Content-Type", "text/plain");
    resp.getWriter().println("Hello Servlet "); // BODY

--- src/main/java/tobyspringboot/helloboot2/Helloboot2Application.java
+++ src/main/java/tobyspringboot/helloboot2/Helloboot2Application.java
@@ -3,6 +3,14 @@ package tobyspringboot.helloboot2;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class Helloboot2Application {
@@ -14,8 +22,24 @@ public class Helloboot2Application {
 
         // 방법 2 스프링 부트가 톰캣 서블릿 컨테이너를 내장해서 코드로 쉽게 사용할수 있게 만든 도우미 클래스가 있다.
         // TomcatServletWebServerFactory의 인스턴스를 생성해서 웹서버를 시작할 수 있다.
+
+        // [서블릿 등록 (Mapping) - 서블릿을 서블릿 컨테이너에 매핑]
+        // getWebServer로 webServer를 만들때에 ServletContextInitializer 에서 서블릿을 등록해준다.
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            // hello라는 서블릿을 등록한다. 첫번째는 서블릿명, 두번째는 HttpServlet을 상속하는 익명 클래스 오브젝트를 만들어서 service 메소드를 구현한다.
+            // service에서는 request, response가 있고 언제나 요청이 있어야 응답이 있다.
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // super.service(req, resp);
+                    // 웹 응답의 3대 요소 리 (상태코드 / 헤더 / 바디)
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet "); // BODY
+                }
+            }).addMapping("/hello"); // 서블릿을 추가할때 서블릿 컨테이너가 요청이 들어올때 어느 서블릿에 매핑해줘야할지 결정하는 매핑을 추가한다.
+        });
         webServer.start();
     }
 
