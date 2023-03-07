f0bfe7f9e92fb8ce8ddffd6916f6251867bf3f6c
changwook yang <ycw92325@gmail.com>
2023-03-07 21:59:39 +0900

    Containerless 개발 준비
    SpringBootApplication 어노테이션과
    SpringApplication.run(Helloboot2Application.class, args) 메소드만으로 Containerless 환경을 구축해냈다.
    
    이제 SpringBootApplication와 SpringApplication.run(Helloboot2Application.class, args)  없이 
    Containerless를 구현하기 위해 위의 소스들을 삭제하였다.
    
    main 메소드를 실행하면 콘솔에 메시지는 찍히지만 톰캣 (웹서버, Servlet Container)은 실행되지 않으며
    Spring boot가 올라가지 않는다.

--- src/main/java/tobyspringboot/helloboot2/Helloboot2Application.java
+++ src/main/java/tobyspringboot/helloboot2/Helloboot2Application.java
@@ -3,11 +3,25 @@ package tobyspringboot.helloboot2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
 @SpringBootApplication
 public class Helloboot2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Helloboot2Application.class, args);
+    }
+
+}
+*/
+/*
+SpringBootApplication 어노테이션과
+SpringApplication.run(Helloboot2Application.class, args) 메소드만으로 Containerless 환경을 구축해냈다.
+이제 SpringBootApplication와 SpringApplication.run(Helloboot2Application.class, args)  없이 구현해보자!!
+*/
+public class Helloboot2Application {
+
+    public static void main(String[] args) {
+        System.out.println("실행해도 콘솔에 찍히지만 톰캣이 올라가지 않으며 Spring boot는 실행되지 않는다.");
     }
 
 }
