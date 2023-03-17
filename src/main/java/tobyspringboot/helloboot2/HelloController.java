83da61175ae30558a6ee878b72e542c889a6928e
changwook yang <ycw92325@gmail.com>
2023-03-17 23:26:52 +0900

    Controller에 DispatcherServlet의 매핑정보 추가
    DispatcherServlet은 웹 요청을 수행할 수 있는 웹 컨트롤러인지를 구분해야한다.
    Method에 GetMapping만 적을 경우 DispatcherServlet이 모든 메소드를 뒤질수는 없으므로
    클래스 레벨에 @RequestMapping이 추가되어야 한다.
    
    String을 Return하게 되면 DispatcherServlet은 기본적으로 View를 리턴받게 되는것으로 인식하는데
    응답에 대한 View가 없으므로 에러가 발생하게 된다.
    View 대신에 Body에 넣어서 동작하게 하려면 @ResponseBody의 추가가 필요하게 된다.
    
    추가적으로 @RestController를 클래스 레벨에 붙이게 되면
    DispatcherServlet은 응답을 View가 아닌 Body에 넣어서 응답하는 것으로 인식한다.
    즉, RestController 어노테이션이 있는 Method들은 @ResponseBody가 추가된것으로 인식한다.

--- src/main/java/tobyspringboot/helloboot2/HelloController.java
+++ src/main/java/tobyspringboot/helloboot2/HelloController.java
@@ -1,7 +1,12 @@
 package tobyspringboot.helloboot2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +14,20 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    // GetMapping("/hello")를 추가하게되면
+    // DispatcherServlet은 Bean을 다 뒤져서 요청정보를 추출해낸다.
+    // DispatcherServlet은 웹 컨트롤러인지 구분을 해야하는데
+    // GetMapping만 적어놓을경우 method레벨이므로 DispatcherServlet이 모든 메소드를 다 뒤질수는 없다.
+    // 따라서 클래스 레벨에도 @RequestMapping이 필요하다.
+    // 여기서 GET /hello를 실행하게되면 Mapping은 정상적으로 되었으나 에러가 발생하게 된다.
+
+    // 바로 String Return 타입일 경우에는 view의 이름을 노출해주는 것으로 DispatcherServlet은 인식하게 된다.
+    // View를 찾으려고 하는데 원하는 뷰가 없어서 에러를 리턴하게 된다.
+    // View 대신에 Body에 넣어서 동작하게 하려면 @ResponseBody의 추가가 필요하다.
+    // @RestController Annotation을 클래스 레벨에 붙이게되면 DispatcherServlet은 응답을 view가 아닌 Body에 넣어서 응답하는 것으로 인식하게 된다.
+    // (@ResponseBody가 붙은것으로 인식한다.)
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
