7f6d58ddf0d7a4f84d9294a0d5d1b85bf4f7e121
changwook yang <ycw92325@gmail.com>
2023-03-28 21:08:39 +0900

    Conditional을 알아야하는 이유
    스프링부트가 제공하는 Conditional에 대해서 알고있어야 하는 이유는
    스프링부트가 제공하는 자동구성을 파악해야하는 경우가 있다.
    
    어떠한 조건을 만족하면 어떠한 빈이 등록되는구나를 알아야 
    해당 빈을 가져다쓰거나 필요한 경우에 Custom Bean을 만들어서 대체할 수 있다.
    
    혹은 스프링에서 제공하지 않는 제 3의 기술을 구현하기 위해서 자동구성 형태로 만들기 위해서는
    어노테이션의 확장에 대해서 잘알고 있어야한다.
    
    + 개발자가 구성한 Bean에 대하여 우선순위가 크다.

--- src/main/java/tobyspringboot/config/MyOnClassCondition.java
+++ src/main/java/tobyspringboot/config/MyOnClassCondition.java
@@ -7,6 +7,16 @@ import org.springframework.util.ClassUtils;
 
 import java.util.Map;
 
+/*
+스프링부트가 제공하는 Conditional에 대해서 알고있어야 하는 이유는
+스프링부트가 제공하는 자동구성을 파악해야하는 경우가 있다.
+
+어떠한 조건을 만족하면 어떠한 빈이 등록되는구나를 알아야 해당 빈을 가져다쓰거나
+필요한 경우에 Custom Bean을 만들어서 대체할 수 있다.
+
+혹은 스프링에서 제공하지 않는 제 3의 기술을 구현하기 위해서 자동구성 형태로 만들기 위해서는
+어노테이션의 확장에 대해서 잘알고 있어야한다.
+*/
 public class MyOnClassCondition implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
--- src/main/java/tobyspringboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspringboot/config/autoconfig/TomcatWebServerConfig.java
@@ -1,15 +1,19 @@
 package tobyspringboot.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import tobyspringboot.config.ConditionalMyOnClass;
 import tobyspringboot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass(value = "org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 개발자가 직접 등록한 Bean이 있는지 확인해보고 없을 경우 등록한다.
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
